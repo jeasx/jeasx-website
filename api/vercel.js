27bcfcccce3eb410f434d281fb9a1f208e2effd8
Maik Jablonski <jablonski@neoskop.de>
2024-01-18 10:34:22 +0100

    testing vercel cold start optimization

--- api/vercel.js
+++ api/vercel.js
@@ -1,6 +1,7 @@
 import serverless from "jeasx/serverless.js";
 
 export default async (req, res) => {
-  await serverless.ready();
   serverless.server.emit("request", req, res);
 };
+
+await serverless.ready();
