468f5b356f521cec0e2d9d04e90db29949e45012
Maik Jablonski <jablonski@neoskop.de>
2024-10-04 17:01:14 +0200

    overflow-auto for Code

--- src/components/Code.jsx
+++ src/components/Code.jsx
@@ -2,7 +2,7 @@ import { escapeEntities } from "jsx-async-runtime";
 
 export default function Code({ source = "", lang = "jsx" }) {
   return (
-    <pre class={`prism-code language-${lang}`}>
+    <pre class={`prism-code language-${lang} overflow-auto`}>
       <code>{escapeEntities(source)}</code>
     </pre>
   );
