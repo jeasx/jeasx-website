04f707bec6790a284a590dcf87d65bb4954d624d
Maik Jablonski <jablonski@neoskop.de>
2025-03-15 15:35:02 +0100

    Use duckduckgo as favicon provider

--- src/routes/favicon/[...path].js
+++ src/routes/favicon/[...path].js
@@ -2,14 +2,12 @@
  * @param {import("../types").RouteProps} props
  */
 export default async function Icon({ request, reply }) {
-  reply.header("Content-Type", "image/png");
+  const domain = request.path.substring("/favicon/".length);
+
+  reply.header("Content-Type", "image/x-icon");
   reply.header("Cache-Control", "public, max-age=31536000, immutable");
   return new Response(
-    (
-      await fetch(
-        `https://icon.horse${request.path.replace("/favicon/", "/icon/")}`,
-      )
-    ).body,
+    (await fetch(`https://duckduckgo.com/ip3/${domain}.ico`)).body,
     {
       status: 200,
     },
