4d704f02653eceda92eb6cf913a60cf1fdc24509
Maik Jablonski <jablonski@neoskop.de>
2025-09-11 12:04:47 +0200

    cache value

--- src/routes/[...guard].js
+++ src/routes/[...guard].js
@@ -12,7 +12,7 @@ export default function ({ request, reply }) {
   }
 
   if (process.env.NODE_ENV !== "development") {
-    reply.header("Cache-Control", "public, max-age=3600, s-maxage=3600");
+    reply.header("Cache-Control", "public,max-age=3600,s-maxage=3600");
   }
 
   // Set the request and reply objects as context
