0cb61c531c793f127595b1f0c185d260ea1e99b5
Maik Jablonski <jablonski@neoskop.de>
2026-02-11 14:15:37 +0100

    removed Code component; converted all pages to MDX

--- src/components/Code.jsx
+++
@@ -1,7 +0,0 @@
-export default function Code({ source = "", lang = "jsx" }) {
-  return (
-    <pre class={`prism-code language-${lang} overflow-auto`}>
-      <code>{source}</code>
-    </pre>
-  );
-}
--- src/components/HeroStage.jsx
+++ src/components/HeroStage.jsx
@@ -1,4 +1,4 @@
-import Code from "./Code";
+import HeroStageCode from "./HeroStageCode.mdx";
 import IconHeroStage from "./icons/IconHeroStage";
 
 export default function HeroStage() {
@@ -79,29 +79,8 @@ export default function HeroStage() {
                       </div>
                     </div>
                   </div>
-                  <div class="flex items-start px-1 text-sm">
-                    <Code
-                      source={`
-export default async function ({ request, reply }) {
-  const id = request.query["id"];
-  const { name, image, ingredients } = await (
-    await fetch(\`https://dummyjson.com/recipes/\${id}\`)
-  ).json();
-
-  return (
-    <article class="recipe">
-      <h1>{name}</h1>
-      <img src={image} alt={name} />
-      <ul>
-        {ingredients.map((ingredient) => (
-          <li>{ingredient}</li>
-        ))}
-      </ul>
-    </article>
-  );
-}
-`}
-                    />
+                  <div class="flex items-start px-1 py-2.5 text-sm overflow-auto">
+                    <HeroStageCode />
                   </div>
                 </div>
               </div>
