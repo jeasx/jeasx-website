22c9fa7ca21573a5704368c388da1ec63e236183
Maik Jablonski <mail@mjablonski.de>
2026-08-04 15:42:13 +0200

    "jeasx": "2.10.0"

--- jeasx.config.js
+++ jeasx.config.js
@@ -7,6 +7,7 @@ const NODE_ENV_IS_DEVELOPMENT = process.env.NODE_ENV === "development";
 export default {
   /** @type {() => import("esbuild").BuildOptions} */
   ESBUILD_SERVER_OPTIONS: () => ({
+    define: { "process.env.BUILD_ID": `"${Date.now().toString(36)}"` },
     loader: { ".svg": "text" },
     plugins: [
       mdxPlugin({
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,7 +2,7 @@ import Header from "./Header";
 import MobileNavigation from "./MobileNavigation";
 import Navigation from "./Navigation";
 
-const BUILD_TIME = process.env.BUILD_TIME;
+const BUILD_ID = process.env.BUILD_ID;
 
 /**
  * @this {import("../types").RouteProps}
@@ -39,10 +39,10 @@ export default function Layout({
             rel="canonical"
             href={`https://www.jeasx.dev${path.endsWith("/") ? path.slice(0, -1) : path}`}
           ></link>
-          <link rel="stylesheet" href={`/index~${BUILD_TIME}.css`} />
-          <script type="module" src={`/index~${BUILD_TIME}.js`}></script>
-          <link rel="stylesheet" href={`/pagefind/pagefind-component-ui.css?${BUILD_TIME}`} />
-          <script type="module" src={`/pagefind/pagefind-component-ui.js?${BUILD_TIME}`}></script>
+          <link rel="stylesheet" href={`/index~${BUILD_ID}.css`} />
+          <script type="module" src={`/index~${BUILD_ID}.js`}></script>
+          <link rel="stylesheet" href={`/pagefind/pagefind-component-ui.css?${BUILD_ID}`} />
+          <script type="module" src={`/pagefind/pagefind-component-ui.js?${BUILD_ID}`}></script>
         </head>
         <body class="flex min-h-full bg-white dark:bg-slate-900">
           <div class="flex w-full flex-col">
